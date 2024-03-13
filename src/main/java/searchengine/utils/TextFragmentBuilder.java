7042fe9621cfcf6b43b8ad887c3b588a7cdb484c
Andrey Puzankov <andrey.puzankov@gazprombank.ru>
2024-03-13 12:03:32 +0300

    Упростил до безобразия, эх

--- src/main/java/searchengine/utils/TextFragmentBuilder.java
+++ src/main/java/searchengine/utils/TextFragmentBuilder.java
@@ -3,10 +3,7 @@ package searchengine.utils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
-import java.util.*;
 
 public class TextFragmentBuilder {
     public static String buildSnippet(String query, String htmlContext) throws IOException {
@@ -14,29 +11,11 @@ public class TextFragmentBuilder {
         return setBold(query, snippet);
     }
 
-    private static String setBold(String query, String text) throws IOException {
-        String snippet = text;
-        Set<String> searchWords = Lemmatizator.init().getLemmaSet(query);
-
-        String[] words = snippet.split("\\s+");
-        Set<String> setToReplace = new HashSet<>();
-
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (searchWords.contains(word.toLowerCase(Locale.ROOT))) {
-                setToReplace.add(word);
-                break;
-            }
-        }
-
-        for (String replString : setToReplace) {
-            snippet = snippet.replaceAll(replString, "<b>" + replString + "</b>");
-        }
-
-        return snippet;
+    private static String setBold(String query, String text)  {
+        return text.toLowerCase().replaceAll(query, "<b>" + query + "</b>");
     }
 
-    private static String getRightSnippet(String query, String htmlContext) throws IOException {
+    private static String getRightSnippet(String query, String htmlContext)  {
         String decodedHtml = StringEscapeUtils.unescapeHtml4(htmlContext);
         Document document = Jsoup.parse(decodedHtml);
         String elements = document.text();
@@ -44,8 +23,7 @@ public class TextFragmentBuilder {
         int searchWord = elements.indexOf(query);
         int pointBefore = elements.lastIndexOf(point, searchWord);
         int pointAfter = elements.indexOf(point, searchWord);
-        String snippet = elements.substring(pointBefore+1, pointAfter);
-        return snippet;
+        return elements.substring(pointBefore+1, pointAfter);
     }
 
 
